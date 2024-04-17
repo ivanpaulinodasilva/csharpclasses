b393018927764b30e9f7a16193b2c67e6b2d0b98
Ivan Paulino <48137161+ivanpaulinodasilva@users.noreply.github.com>
2024-04-17 17:33:44 -0300

    update

--- pontoFlutuante/Program.cs
+++ pontoFlutuante/Program.cs
@@ -1,13 +1,13 @@
 ﻿/*int[] data;
 data = new int[3];
 //simplificando uma linha de código
-
+*/
 int[] data = new int[3];
-
+//criar um novo comentario
 Console.WriteLine("");
 Console.WriteLine("Floating point types:");
 Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
 Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
 Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
-*/
+
 
