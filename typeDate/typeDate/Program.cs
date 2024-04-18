a975791ca2677b40b40dad92c83a5a39e0cc9528
Ivan Paulino <48137161+ivanpaulinodasilva@users.noreply.github.com>
2024-04-18 15:54:08 -0300

    concluido

--- typeDate/typeDate/Program.cs
+++ typeDate/typeDate/Program.cs
@@ -123,3 +123,61 @@ if (result > 0)
    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
 
 */
+
+/*Exercício – Concluir um desafio em que é preciso combinar 
+valores de matriz de cadeia de caracteres como cadeias de 
+caracteres e inteiros.*/
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal total = 0m;
+string message = "";
+
+foreach (var value in values)
+{
+    decimal number; // stores the TryParse "out" value
+    if (decimal.TryParse(value, out number))
+    {
+        total += number;
+    } else
+    {
+        message += value;
+    }
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
+
+
+
+/*int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// Your code here to set result2
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+
+/// SOLUTION
+
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// The Convert class is best for converting the fractional decimal numbers into whole integer numbers
+// Convert.ToInt32() rounds up the way you would expect.
+int result1 = Convert.ToInt32(value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");*/
